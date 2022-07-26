b7b0eae5233c2b4e2bfa4b272c825d2bdde6d46a
Ostoja24 <86255154+Ostoja24@users.noreply.github.com>
2022-07-26 19:39:14 +0200

    Update custom_fixtures.py

--- tests/custom_fixtures.py
+++ tests/custom_fixtures.py
@@ -1,11 +1,11 @@
 import pytest
 from pathlib import Path
 from selenium import webdriver
-
+# FIXTURE WITH SETUP WEBDRIVER
 @pytest.fixture(scope="class")
 def chrome_setup():
     options = webdriver.ChromeOptions()
-    path = Path("C:/TestFiles/chromedriver.exe")
+    path = Path("C:/TestFiles/chromedriver.exe") # PLEASE REPLACE HERE YOUR PATH TO DRIVER ON LOCAL DISK
     driver = webdriver.Chrome(executable_path=path, options = options)
     options.add_argument("--no-sandbox")
     options.add_argument("--maximize-window")
